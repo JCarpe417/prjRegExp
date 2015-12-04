7610fccae3f61dfa0eee091facf9a11151327d6e
JCarpe417 <JCarpe417@gmail.com>
2015-12-04 08:25:10 -0500

    1st Commit
    1st commit to master

---
+++ prjRegExp/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace prjRegExp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRegExp = new System.Windows.Forms.TextBox();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblMatches = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.mnuTest = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuSingle = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBlock = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBlock = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.mnuTest.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(101, 12);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(426, 20);
+            this.txtText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text to Test: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RegExp to Test: ";
+            // 
+            // txtRegExp
+            // 
+            this.txtRegExp.Location = new System.Drawing.Point(101, 129);
+            this.txtRegExp.Name = "txtRegExp";
+            this.txtRegExp.Size = new System.Drawing.Size(426, 20);
+            this.txtRegExp.TabIndex = 3;
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(101, 155);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(426, 95);
+            this.lstResults.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Results: ";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(20, 199);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 6;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnTest_MouseDown);
+            // 
+            // lblMatches
+            // 
+            this.lblMatches.AutoSize = true;
+            this.lblMatches.Location = new System.Drawing.Point(58, 171);
+            this.lblMatches.Name = "lblMatches";
+            this.lblMatches.Size = new System.Drawing.Size(19, 13);
+            this.lblMatches.TabIndex = 7;
+            this.lblMatches.Text = "(0)";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(20, 227);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // mnuTest
+            // 
+            this.mnuTest.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSingle,
+            this.mnuBlock,
+            this.mnuFile});
+            this.mnuTest.Name = "mnuTest";
+            this.mnuTest.Size = new System.Drawing.Size(174, 70);
+            // 
+            // mnuSingle
+            // 
+            this.mnuSingle.Name = "mnuSingle";
+            this.mnuSingle.Size = new System.Drawing.Size(173, 22);
+            this.mnuSingle.Text = "Test on Single Line";
+            this.mnuSingle.Click += new System.EventHandler(this.mnuSingle_Click);
+            // 
+            // mnuBlock
+            // 
+            this.mnuBlock.Name = "mnuBlock";
+            this.mnuBlock.Size = new System.Drawing.Size(173, 22);
+            this.mnuBlock.Text = "Test on Block";
+            this.mnuBlock.Click += new System.EventHandler(this.mnuBlock_Click);
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(173, 22);
+            this.mnuFile.Text = "Test on File";
+            this.mnuFile.Click += new System.EventHandler(this.mnuFile_Click);
+            // 
+            // txtBlock
+            // 
+            this.txtBlock.Location = new System.Drawing.Point(101, 38);
+            this.txtBlock.Multiline = true;
+            this.txtBlock.Name = "txtBlock";
+            this.txtBlock.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBlock.Size = new System.Drawing.Size(426, 85);
+            this.txtBlock.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Block to Test: ";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(20, 100);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 13;
+            this.btnLoad.Text = "Load Into ->";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 262);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBlock);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblMatches);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.txtRegExp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Regular Expression Tester";
+            this.mnuTest.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRegExp;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblMatches;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ContextMenuStrip mnuTest;
+        private System.Windows.Forms.ToolStripMenuItem mnuSingle;
+        private System.Windows.Forms.ToolStripMenuItem mnuBlock;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.TextBox txtBlock;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ prjRegExp/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace prjRegExp
+{
+    /*===============================================================
+     | Creator: James Carpenter (jcarpe417@gmail.com)                |
+     | Project: Regular Expression Tester across different mediums   |
+      ===============================================================*/
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //boolean function to test for an empty regex
+        private bool isExp() { if (this.txtRegExp.Text == "") return false; else return true; }
+
+        private void btnTest_MouseDown(object sender, MouseEventArgs e)
+        {
+            //show test menu at mouses point(e.X,e.Y) relative to the test button
+            this.mnuTest.Show(this.btnTest, e.X, e.Y);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear everything user has entered and results from regexes
+            this.txtText.Clear();
+            this.txtBlock.Clear();
+            this.txtRegExp.Clear();
+            this.lstResults.Items.Clear();
+            this.lblMatches.Text = "(0)";
+            this.txtText.Focus();
+        }
+
+        private void mnuSingle_Click(object sender, EventArgs e)
+        {
+            //if there's no expression, exit procedure
+            if (!this.isExp()) { return; }
+
+            //str: string used to test with the regex
+            //regExp: string representation of a regex pattern
+            string str = this.txtText.Text;
+            string regExp = this.txtRegExp.Text;
+
+            //matches: collection of matches generated from the regex test
+            MatchCollection matches = new Regex(regExp).Matches(str);
+
+            //no matches turned up, do this...
+            if (matches.Count < 1)
+            {
+                //inform the user, then exit procedure
+                MessageBox.Show("No matches came from the expression: " + regExp, "Error");
+                return;
+            }
+
+            //for every match in the collection, do this...
+            foreach (Match m in matches)
+            {
+                //add the value to the results listbox
+                this.lstResults.Items.Add(m.Value);
+            }
+
+            //update result count
+            this.lblMatches.Text = "(" + this.lstResults.Items.Count.ToString() + ")";
+        }
+
+        private void mnuFile_Click(object sender, EventArgs e)
+        {
+            //if there's no expression, exit procedure
+            if (!this.isExp()) { return; }
+
+            //create new file select dialog object to select a file
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //user chose a file and selected Open (OK)
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //regex: regex pattern to use for testing
+                //sr: streamreader object set to file chosen by user
+                Regex regex = new Regex(this.txtRegExp.Text);
+                StreamReader sr = new StreamReader(ofd.FileName);
+
+                //until EOF is reached, do this...
+                while (sr.Peek() != -1)
+                {
+                    //matches: collection of matches generated from the regex test
+                    MatchCollection matches = regex.Matches(sr.ReadLine());
+
+                    //for each match in the collection, do this...
+                    foreach (Match m in matches)
+                    {
+                        //add the value to the results listbox
+                        this.lstResults.Items.Add(m.Value);
+                    }
+                }
+
+                //update result count
+                this.lblMatches.Text = "(" + this.lstResults.Items.Count.ToString() + ")";
+                //properly release streamreader object
+                sr.Close();
+            }
+        }
+
+        private void mnuBlock_Click(object sender, EventArgs e)
+        {
+            //if there's no expression, exit procedure
+            if (!this.isExp()) { return; }
+
+            //regex: regex pattern to use for testing
+            Regex regex = new Regex(this.txtRegExp.Text);
+
+            //for each line in txtBlock.Lines collection, do this...
+            foreach (string line in this.txtBlock.Lines)
+            {
+                //matches: collection of matches generated from the regex test
+                MatchCollection matches = regex.Matches(line);
+
+                //for each match in the collection, do this...
+                foreach (Match m in matches)
+                {
+                    //add the value to the results listbox
+                    this.lstResults.Items.Add(m.Value);
+                }
+            }
+
+            //update result count
+            this.lblMatches.Text = this.lstResults.Items.Count.ToString();
+        }
+
+        //TODO: This may need adjusting
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //create new file select dialog and set filter property for .txt and .csv files
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files|*.txt|Comma Seperated Value (CSV) Files|*.csv";
+
+            //user chose a file and clicked open
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //which file type was selected?
+                switch (ofd.FilterIndex)
+                { 
+                    //text file selected
+                    case 1:
+                        //sr: streamreader object set to file chosen by the user
+                        StreamReader sr = new StreamReader(ofd.FileName);
+
+                        //until EOF is reached, do this...
+                        while (sr.Peek() != -1)
+                        {
+                            //read sr line by line and update txtBlock.Text accordingly
+                            this.txtBlock.Text += sr.ReadLine() + Environment.NewLine;
+                        }
+
+                        //properly release streamreader object
+                        sr.Close();
+                        break;
+                    
+                    //csv file selected
+                    case 2:
+                        //stream: streamreader object set to file chosen by user
+                        StreamReader stream = new StreamReader(ofd.FileName);
+
+                        //until EOF is reached, do this...
+                        while (stream.Peek() != -1)
+                        {
+                            //split lines into strings seperated by commas
+                            string[] strs = stream.ReadLine().Split(',');
+
+                            //for each string in the array
+                            foreach (string s in strs)
+                            {
+                                //update txtBlock.Text accordingly
+                                this.txtBlock.Text += s + Environment.NewLine;
+                            }
+                        }
+
+                        //properly release streamreader object
+                        stream.Close();
+                        break;
+                }
+            }
+        }
+    }
+}
